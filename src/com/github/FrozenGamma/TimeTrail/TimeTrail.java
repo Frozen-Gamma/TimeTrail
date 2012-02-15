ffe016cf2573b02ca85511a7cd0cb1859a150d07
Frozen-Gamma <FrozenGamma@gmail.com>
2012-02-15 16:00:06 +0100

    Adding Eclipse files

---
+++ src/com/github/FrozenGamma/TimeTrail/TimeTrail.java
@@ -0,0 +1,13 @@
+package com.github.FrozenGamma.TimeTrail;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TimeTrail extends JavaPlugin{
+	public void onEnable(){
+	
+	}
+	
+	public void onDisable(){
+		
+	}
+}
