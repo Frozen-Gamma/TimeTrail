fc85969995795dc28bca05080bf486ca7257b7ee
Frozen-Gamma <FrozenGamma@gmail.com>
2012-02-15 20:07:18 +0100

    Added PlayerInteractEvent and added message when right-clicking on a sign.

--- src/com/github/FrozenGamma/TimeTrail/TimeTrail.java
+++ src/com/github/FrozenGamma/TimeTrail/TimeTrail.java
@@ -1,17 +1,24 @@
 package com.github.FrozenGamma.TimeTrail;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.logging.Logger;
 
 public class TimeTrail extends JavaPlugin{
 	
+	public final SignListener signListener = new SignListener();
+	
 	Logger log = Logger.getLogger("Minecraft");
 	
 	public void onEnable(){
-		log.info("TimeTrail has been enabled.");
+		PluginManager pm = getServer().getPluginManager();
+		
+		pm.registerEvents(this.signListener, this);
 	}
 	
 	public void onDisable(){
-		log.info("TimeTrail has been disabled.");
+		
 	}
+
 }
