18477652cf2c2347be78dc56f4da8c4fd8dfb44f
Frozen-Gamma <FrozenGamma@gmail.com>
2012-03-05 22:03:00 +0100

    Reset the time when logging back in. And some code cleanup. (Handling things in a better way)

--- src/com/github/FrozenGamma/TimeTrail/SignListener.java
+++ src/com/github/FrozenGamma/TimeTrail/SignListener.java
@@ -12,55 +12,50 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.block.Sign;
 
 public class SignListener extends JavaPlugin implements Listener
 {
 	public static Player[] playersOnline;
-	static Player player;
+	static Player player, playerLogin;
 	public static Map<Player, Boolean> counting = new HashMap<Player, Boolean>();
 	public static Map<Player, Integer> ticks = new HashMap<Player, Integer>();
 	static Logger log = Logger.getLogger("Minecraft");
 	
 	@EventHandler
+	public static void playerLogin(PlayerLoginEvent event)
+	{
+		playerLogin = event.getPlayer();
+		counting.put(playerLogin, false);
+		ticks.put(playerLogin, 0);
+	}
+	
+	@EventHandler
 	public static void signListener(PlayerInteractEvent event)
 	{
 		player = (Player) event.getPlayer();
-		Action action = event.getAction();
 		Block block = event.getClickedBlock();
 		
-		if(action == Action.RIGHT_CLICK_BLOCK && (block.getType() == Material.SIGN || (block.getType() == Material.SIGN_POST || block.getType() == Material.WALL_SIGN))) 
+		if(event.getAction() == Action.RIGHT_CLICK_BLOCK && (block.getType() == Material.SIGN || (block.getType() == Material.SIGN_POST || block.getType() == Material.WALL_SIGN))) 
 		{
-			try
-			{
-				Sign s = (Sign) event.getClickedBlock().getState();
-				String signText = s.getLine(0);
-		    	if(signText.equalsIgnoreCase("[TimeTrail]"))
-		    	{
-		    		if(!counting.get(player))
-		    		{
-		    			ticks.put(player, 0);
-		    			counting.put(player, true);
-		    		}
-		    		else
-		    		{
-		    			counting.put(player, false);
-		    			log.info(ticks.toString());
-		    		}
-		    	}
-			}
-			catch (Exception e)
-			{
-				counting.put(player, false);
-				Sign s = (Sign) event.getClickedBlock().getState();
-				String signText = s.getLine(0);
-		    	if(signText.equalsIgnoreCase("[TimeTrail]"))
+			Sign s = (Sign) event.getClickedBlock().getState();
+			String signText = s.getLine(0);
+		    
+			if(signText.equalsIgnoreCase("[TimeTrail]"))
+		    {
+		    	if(!counting.get(player))
 		    	{
 		    		ticks.put(player, 0);
 		    		counting.put(player, true);
 		    	}
-			}
+		    	else
+		    	{
+		    		counting.put(player, false);
+		    		log.info(ticks.toString());
+		    	}
+		    }
 		}
 	}
 
@@ -69,7 +64,7 @@ public class SignListener extends JavaPlugin implements Listener
 		playersOnline = (Bukkit.getOnlinePlayers());
 		for(Player p : playersOnline)
 		{
-			if(counting.get(p) != null && counting.get(p))
+			if(counting.get(p))
 			{
 				ticks.put(p, ticks.get(p) + 1);
 			}
