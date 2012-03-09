ceed8735301b17b75d26d12b11ac83c3cbf23f2d
Frozen-Gamma <FrozenGamma@gmail.com>
2012-03-09 14:22:05 +0100

    Version 1.0 released. Read the readme for changelog/information.

--- src/com/github/FrozenGamma/TimeTrail/SignListener.java
+++ src/com/github/FrozenGamma/TimeTrail/SignListener.java
@@ -24,6 +24,7 @@ public class SignListener extends JavaPlugin implements Listener
 	static Player player, playerLogin, playerDeath;
 	public static Map<Player, Boolean> counting = new HashMap<Player, Boolean>();
 	public static Map<Player, Double> ticks = new HashMap<Player, Double>();
+	public static Map<Player, String> TrackName = new HashMap<Player, String>();
 	static Logger log = Logger.getLogger("Minecraft");
 	
 	@EventHandler
@@ -32,6 +33,7 @@ public class SignListener extends JavaPlugin implements Listener
 		playerLogin = event.getPlayer();
 		counting.put(playerLogin, false);
 		ticks.put(playerLogin, 0.00);
+		TrackName.put(playerLogin, null);
 	}
 	
 	@EventHandler
@@ -43,6 +45,7 @@ public class SignListener extends JavaPlugin implements Listener
 			playerDeath.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] You died, counting has stopped.");
 			counting.put(playerDeath, false);
 			ticks.put(playerDeath, 0.00);
+			TrackName.put(playerDeath, null);
 		}
 	}
 	
@@ -55,22 +58,50 @@ public class SignListener extends JavaPlugin implements Listener
 		if(event.getAction() == Action.RIGHT_CLICK_BLOCK && (block.getType() == Material.SIGN || (block.getType() == Material.SIGN_POST || block.getType() == Material.WALL_SIGN))) 
 		{
 			Sign s = (Sign) event.getClickedBlock().getState();
-			String signText = s.getLine(0);
+			String signTextLine1 = s.getLine(0);
+			String signTextLine2 = s.getLine(1);
+			String signTextLine3 = s.getLine(2);
 		    
-			if(signText.equalsIgnoreCase("[TimeTrail]"))
+			if(signTextLine1.equalsIgnoreCase("[TimeTrail]"))
 		    {
-		    	if(!counting.get(player))
-		    	{
-		    		ticks.put(player, 0.00);
-		    		counting.put(player, true);
-		    		player.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] Counting has begun.");
-		    	}
-		    	else
-		    	{
-		    		counting.put(player, false);
-		    		log.info(ticks.toString());
-		    		player.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] Your time was: " + ChatColor.RED + ticks.get(player) / 20 + ChatColor.WHITE + " seconds.");
-		    	}
+				if(!signTextLine2.isEmpty() && (signTextLine3.equalsIgnoreCase("Begin") || signTextLine3.equalsIgnoreCase("End")))
+				{
+					if(TrackName.get(player) == null && signTextLine3.equalsIgnoreCase("Begin"))
+					{
+						TrackName.put(player, signTextLine2);
+						ticks.put(player, 0.00);
+						counting.put(player, true);
+						player.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] Counting has begun.");
+					}
+					else if(signTextLine2.equalsIgnoreCase(TrackName.get(player)) && signTextLine3.equalsIgnoreCase("End"))
+					{
+						TrackName.put(player, null);
+						counting.put(player, false);
+						player.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] Your time was: " + ChatColor.RED + ticks.get(player) / 20 + ChatColor.WHITE + " seconds.");
+					}
+					else if(signTextLine2.equalsIgnoreCase(TrackName.get(player)) && signTextLine3.equalsIgnoreCase("Begin"))
+					{
+						TrackName.put(player, null);
+					    counting.put(player, false);
+						player.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] Counting has stopped.");
+					}
+					else if(TrackName.get(player) == null && signTextLine3.equalsIgnoreCase("End"))
+					{
+						player.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] You haven't started counting.");
+					}
+					else if(!signTextLine2.equalsIgnoreCase(TrackName.get(player)) && signTextLine3.equalsIgnoreCase("Begin"))
+					{
+						player.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] You're already doing track: " + TrackName.get(player));
+					}
+					else if(!signTextLine2.equalsIgnoreCase(TrackName.get(player)) && signTextLine3.equalsIgnoreCase("End"))
+					{
+						player.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] This is the wrong ending.");
+					}
+				}
+				else
+				{
+					player.sendMessage("[" + ChatColor.RED + "TIMETRAIL" + ChatColor.WHITE + "] Invalid sign.");
+				}
 		    }
 		}
 	}
